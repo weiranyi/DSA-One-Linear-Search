21afcde88a2098eb7d8d5845786206c3987bc0d4
yiweiran <168578203@qq.com>
2021-04-09 17:47:01 +0800

    练习了对算法性能分析的方法，增加了README.md的描述信息

---
+++ src/main/java/com/github/weiranyi/LinaerSearch/ArrayGenerator.java
@@ -0,0 +1,21 @@
+package com.github.weiranyi.LinaerSearch;
+
+/**
+ * @author: https://github.com/weiranyi
+ * @description 数组生成器
+ * @date: 2021/4/9 5:24 下午
+ * @Version 1.0
+ */
+public class ArrayGenerator {
+    private ArrayGenerator() {
+    }
+
+    public static Integer[] generateOrderedArray(int n) {
+        // 开辟用户制定的数组个数
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++)
+            arr[i] = i;
+        return arr;
+    }
+}
+
--- src/test/java/com/github/weiranyi/SmokeTest.java
+++ src/test/java/com/github/weiranyi/SmokeTest.java
@@ -1,5 +1,6 @@
 package com.github.weiranyi;
 
+import com.github.weiranyi.LinaerSearch.ArrayGenerator;
 import com.github.weiranyi.LinaerSearch.LinearSearch;
 import com.github.weiranyi.LinaerSearch.Student;
 import org.junit.jupiter.api.Test;
@@ -7,13 +8,26 @@ import org.junit.jupiter.api.Test;
 public class SmokeTest {
     @Test
     public void test() {
-        // 测试LinearSearch中的线性查找算法
-        Integer[] data = {15, 19, 32, 8191};
-        int result1 = LinearSearch.search(data, 19);
-        System.out.println(result1);
-        int result2 = LinearSearch.search(data, 9);
-        System.out.println(result2);
-
+        /**
+         * 1、测试：通过数组测试
+         */
+        // 设置问题规模
+        int[] dataSize = {1000, 1000_000};
+        for (int n:dataSize) {
+            // 测试LinearSearch中的线性查找算法
+            Integer[] data = ArrayGenerator.generateOrderedArray(n);
+            long startTime = System.nanoTime();
+            // 通过循环进行多次查找会让性能稳定
+            for (int k = 0; k < 100; k++) {
+                LinearSearch.search(data,n);
+            }
+            long endTime = System.nanoTime();
+            double time = (endTime - startTime) / 1000000000.0;
+            System.out.println("对于"+n+"个数据"+"，运行100次，耗时："+time + "s" +";");
+        }
+        /**
+         * 2、测试：通过自定义类测试
+         */
         // 学生类的测试用例
         Student[] students = {
                 new Student("亦蔚然1"),
